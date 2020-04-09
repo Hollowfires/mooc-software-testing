cbbce287bd61fd74612f1a39ae07dc52741fe1f6
Hollowfires <hollowfires@hotmail.com>
2020-04-09 16:30:27 -0700

    Complete the test class for addFive

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -1,27 +1,43 @@
 package tudelft.gettingstarted;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
 public class GettingStartedTest {
+	
+	GettingStarted g;
+	
+	@BeforeEach
+	public void setup(){
+		g = new GettingStarted();
+	}
 
     @Test
     public void addFiveTo20() {
-        int result = new GettingStarted().addFive(20);
+        int result = g.addFive(20);
         Assertions.assertEquals(25,result);
     }
 
 //    UNCOMMENT THE CODE BELOW, AND FILL THE GAPS!
 
-//    @Test
-//    public void addFiveToZero() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(???, result);
-//    }
-//
-//    @Test
-//    public void addFiveToMinus20() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(????,result);
-//    }
+    @Test
+    public void addFiveToZero() {
+        int result = g.addFive(0);
+        Assertions.assertEquals(5, result);
+    }
+
+    @Test
+    public void addFiveToMinus20() {
+        int result = new GettingStarted().addFive(-20);
+        Assertions.assertEquals(-15,result);
+    }
+    
+    @Test
+    public void testAddingFiveToMax() {
+    	int result = new GettingStarted().addFive(Integer.MAX_VALUE);
+    	Assertions.assertEquals(-2147483644, result);
+    	//System.out.print(Integer.MAX_VALUE + 5);
+    }
 }
